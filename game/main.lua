cdd0bced8e1ad3bdf34de7f60014125b9418c86d
Kazuo256 <kazuo256@gmail.com>
2017-10-30 10:56:22 -0200

    Add libraries to path

--- game/main.lua
+++ game/main.lua
@@ -1,3 +1,7 @@
 
-
+function love.load()
+  local fs = love.filesystem
+  fs.setRequirePath("lib/?.lua;lib/?/init.lua;" .. fs.getRequirePath())
+  package.cpath = fs.getSourceBaseDirectory() .. "/?.so;" .. package.cpath
+end
 
