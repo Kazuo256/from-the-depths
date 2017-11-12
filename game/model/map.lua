28687f369861875bde5773490842402b516d5a24
Kazuo256 <kazuo256@gmail.com>
2017-11-11 22:47:52 -0200

    Move camp stuff to stage

--- game/model/map.lua
+++ game/model/map.lua
@@ -4,8 +4,6 @@ local Map = require 'lux.class' :new{}
 
 local setfenv = setfenv
 local print   = print
-local ipairs  = ipairs
-local table   = table
 
 function Map:instance(_obj, _specname)
 
@@ -40,18 +38,6 @@ function Map:instance(_obj, _specname)
 
   function tilespec(i,j)
     return _tiles[_index(i,j)].spec
-  end
-
-  --[[ Base Camps ]]--
-
-  local _camps  = {}
-
-  local function _addCamp(spec)
-    table.insert(_camps, { pos = spec['pos'], delay = spec['delay'] })
-  end
-
-  for _,campspec in ipairs(_spec['camps']) do
-    _addCamp(campspec)
   end
 
 end
--- game/model/stage.lua
+++ game/model/stage.lua
@@ -5,6 +5,8 @@ local Stage = require 'lux.class' :new{}
 
 local setfenv = setfenv
 local print   = print
+local ipairs  = ipairs
+local table   = table
 
 function Stage:instance(_obj, _specname)
 
@@ -12,11 +14,28 @@ function Stage:instance(_obj, _specname)
 
   _specname     = 'stages/' .. _specname
   local _spec   = DB.load(_specname)
+
+  --[[ Tile Map ]]--
+
   local _map    = Map(_spec['map'])
 
   function map()
     return _map
   end
+
+  --[[ Base Camps ]]--
+
+  local _camps  = {}
+
+  local function _addCamp(spec)
+    table.insert(_camps, { pos = spec['pos'], delay = spec['delay'] })
+  end
+
+  for _,campspec in ipairs(_spec['camps']) do
+    _addCamp(campspec)
+  end
+
+  --[[ Overall Logic ]]--
 
   function update()
   end
