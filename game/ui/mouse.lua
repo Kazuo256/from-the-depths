dd824977363c483abd3c1b9aebd009d1e75e1ec0
Kazuo256 <kazuo256@gmail.com>
2017-11-12 17:02:53 -0200

    Mouse helper module

---
+++ game/ui/mouse.lua
@@ -0,0 +1,25 @@
+
+local vec2  = require 'cpml' .vec2
+local MOUSE = {}
+
+local _mouse_down = { false, false }
+local _mouse_clicked = { false, false }
+
+function MOUSE.update(dt)
+  for i=1,2 do
+    local last = _mouse_down[i]
+    _mouse_down[i] = love.mouse.isDown(i)
+    _mouse_clicked[i] = _mouse_down[i] and not last
+  end
+end
+
+function MOUSE.pos()
+  return vec2(love.mouse.getPosition())
+end
+
+function MOUSE.clicked(i)
+  return _mouse_clicked[i]
+end
+
+return MOUSE
+
