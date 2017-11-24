be4595f931fb1781d1617a60234a7447a5623797
Kazuo256 <kazuo256@gmail.com>
2017-11-24 17:43:16 -0200

    Window border

--- game/conf.lua
+++ game/conf.lua
@@ -11,7 +11,7 @@ function love.conf(t)
   t.window.icon = nil
   t.window.width = 1280
   t.window.height = 720
-  t.window.borderless = true
+  t.window.borderless = false
   t.window.resizable = false
   t.window.minwidth = 1
   t.window.minheight = 1
